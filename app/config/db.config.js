d098379370f779ee463c32001f24f9da0e5ed86e
keerthana <lnu.ke@northeastern.edu>
2023-10-25 16:52:41 -0400

    Commit  changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,7 +5,7 @@ module.exports = {
   USER: process.env.USER,
   PASSWORD: process.env.PASSWORD,
   DB: process.env.DB,
-  dialect: process.env.DIALECT,
+  DIALECT: "mysql",
   pool: {
     max: parseInt(process.env.POOL_MAX),
     min: parseInt(process.env.POOL_MIN),
