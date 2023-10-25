4560db58b827a239c17c7820913f9c9657e6b455
keerthana <lnu.ke@northeastern.edu>
2023-10-25 01:42:10 -0400

    Commit  changes

--- app/models/assignment.model.js
+++ app/models/assignment.model.js
@@ -1,41 +1,47 @@
 module.exports = (sequelize, Sequelize) => {
-  const Assignment = sequelize.define("Assignment", {
-    id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      primaryKey: true,
-      readOnly: true,
+  const Assignment = sequelize.define(
+    "Assignment",
+    {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        readOnly: true,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      points: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      num_of_attemps: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      deadline: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      assignment_created: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false,
+      },
+      assignment_updated: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false,
+      },
     },
-    user_id: {
-      type: Sequelize.UUID,
-      allowNull: false,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    points: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-    num_of_attemps: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-    deadline: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-    assignment_created: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-      allowNull: false,
-    },
-    assignment_updated: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: false, // This will prevent Sequelize from adding createdAt and updatedAt
+    }
+  );
   return Assignment;
 };
