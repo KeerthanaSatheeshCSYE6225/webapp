7fa8918ddf9cf4cec89e3925cc529c8d0ea23868
keerthana <lnu.ke@northeastern.edu>
2023-10-26 17:10:26 -0400

    commit changes to systemd

--- packer/script.sh
+++ packer/script.sh
@@ -2,46 +2,11 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# sudo apt-get update
-# sudo apt-get -y install mariadb-server
-
-# sudo systemctl enable mariadb
-# sudo systemctl start mariadb
-# sudo mysql_secure_installation <<EOF
-
-# n
-# Karan@123
-# Karan@123
-# y
-# y
-# y
-# y
-# EOF
-
-
-# # sudo mysql -u root -pKaran@123<<EOF
-# # CREATE USER 'keerthana'@'localhost' IDENTIFIED BY 'Karan@123';
-# # GRANT ALL PRIVILEGES ON *.* TO 'keerthana'@'localhost';
-# # FLUSH PRIVILEGES;
-# # CREATE DATABASE cloud_db;
-# # USE cloud_db;
-# # EOF
-
-# sudo mysql -u root -pKaran@123 -e "CREATE USER 'keerthana'@'localhost' IDENTIFIED BY 'Karan@123';"
-# sudo mysql -u root -pKaran@123 -e "CREATE DATABASE cloud_db;"
-# sudo mysql -u root -pKaran@123 -e "GRANT ALL PRIVILEGES ON cloud_db.* TO 'keerthana'@'localhost';"
-# sudo mysql -u root -pKaran@123 -e "GRANT CREATE ON cloud_db.* TO 'keerthana'@'localhost';"
-
-# sudo systemctl restart mariadb
-
-
-
-#!/bin/bash
 
 # Install MySQL
 sudo apt-get install -y nodejs npm unzip
 sudo apt install -y mariadb-server
-
+sudo apt install unzip
 
 # Create a database
 sudo mysql -u root -pKaran@123 <<EOF
@@ -55,25 +20,22 @@ GRANT ALL PRIVILEGES ON cloud_db.* TO 'root'@'localhost' IDENTIFIED BY 'Karan@12
 FLUSH PRIVILEGES;
 EOF
 
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo apt install -y nodejs npm unzip
+cd /opt || exit
+mkdir csye6225
+sudo mv ~/webapp1.zip /opt/csye6225/webapp1.zip
 
+cd /opt/csye6225 || exit
+sudo unzip -o webapp.zip -d webapp
+sudo mv /opt/csye6225/webapp/users.csv /opt/csye6225/users.csv
+cd /opt/csye6225/webapp || exit
+sudo -u csye6225 npm install
 
-sudo apt install unzip
-# sudo unzip /home/admin/webapp1.zip
-# sudo mv -i /home/admin/webapp1 root@67.205.157.186:/opt/
-# sudo mkdir /home/webapp
-# sudo mv /tmp
-
-sudo mkdir /home/admin/webapp
-sudo mv /home/admin/webapp1.zip /home/admin/webapp/
-cd webapp/ || exit
-sudo unzip webapp1.zip
-sudo npm i
-
-source_path="/home/admin/webapp/users.csv"
-destination_path="/opt/"
-
-
-# Move the file if it exists
-[ -e "$source_path" ] && sudo mv "$source_path" "$destination_path" && echo "File 'users.csv' moved to '$destination_path'"
-
-
+sudo cp /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
+sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
+sudo chmod -R 750 /opt/csye6225/webapp
+systemctl daemon-reload
+sudo systemctl enable webapp
+sudo systemctl start webapp
