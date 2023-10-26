12056dda65c7461d6c878dc92fe7d82b2fc89aae
keerthana <lnu.ke@northeastern.edu>
2023-10-26 19:33:33 -0400

    checks

--- packer/script.sh
+++ packer/script.sh
@@ -20,25 +20,24 @@ GRANT ALL PRIVILEGES ON cloud_db.* TO 'root'@'localhost' IDENTIFIED BY 'Karan@12
 FLUSH PRIVILEGES;
 EOF
 
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-sudo mkdir /opt/csye6225/webapp
-sudo mv /home/admin/webapp1.zip /opt/csye6225/webapp/
-cd /opt/csye6225/webapp/ || exit
-sudo unzip webapp1.zip
-sudo npm audit fix
-sudo npm install
+sudo apt install -y nodejs npm unzip
  
-source_path="/opt/csye6225/webapp/users.csv"
-destination_path="/opt/"
- 
-# Move the file if it exists
-[ -e "$source_path" ] && sudo mv "$source_path" "$destination_path" && echo "File 'users.csv' moved to '$destination_path'"
- 
-sudo mv /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
- 
-sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/
-sudo chmod -R 750 /opt/csye6225/webapp/
-systemctl daemon-reload
+sudo mv ~/webapp1.zip /opt/csye6225/webapp1.zip
+sudo mv ~/users.csv /opt/csye6225/users.csv
+cd /opt/csye6225 || exit
+sudo unzip -o webapp1.zip -d webapp
+sudo cp /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
+cd webapp || exit
+touch .env
+sudo npm i
+sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
+sudo chmod -R 750 /opt/csye6225/webapp
+sudo systemctl daemon-reload
 sudo systemctl enable webapp
 sudo systemctl start webapp
+
+ 
