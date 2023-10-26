39c2b7f9659fa8170fb9feab1819338809f0da52
Keerthana Satheesh <113073502+Keerthana734@users.noreply.github.com>
2023-10-26 19:01:31 -0400

    Assign6makeup (#32)
    * commit changes to systemd
    
    * message
    
    * message
    
    * commit changes to systemd
    
    * message
    
    * Assign6makeup (#29)
    
    * commit changes to systemd
    
    * message
    
    * message
    
    * commit changes to systemd
    
    * message
    
    * message

--- packer/script.sh
+++ packer/script.sh
@@ -22,21 +22,23 @@ EOF
 
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-sudo apt install -y nodejs npm unzip
-cd /opt || exit
-mkdir csye6225
-sudo mv ~/webapp.zip /opt/csye6225/webapp.zip
-
-
-cd /opt/csye6225 || exit
-sudo unzip -o webapp.zip -d webapp
-sudo mv /opt/csye6225/webapp/users.csv /opt/csye6225/users.csv
-cd /opt/csye6225/webapp || exit
-sudo -u csye6225 npm install
-
-sudo cp /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
-sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
-sudo chmod -R 750 /opt/csye6225/webapp
+sudo mkdir /opt/csye6225/webapp
+sudo mv /home/admin/webapp1.zip /opt/csye6225/webapp/
+cd /opt/csye6225/webapp/ || exit
+sudo unzip webapp1.zip
+sudo npm audit fix
+sudo npm install
+ 
+source_path="/opt/csye6225/webapp/users.csv"
+destination_path="/opt/"
+ 
+# Move the file if it exists
+[ -e "$source_path" ] && sudo mv "$source_path" "$destination_path" && echo "File 'users.csv' moved to '$destination_path'"
+ 
+sudo mv /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
+ 
+sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/
+sudo chmod -R 750 /opt/csye6225/webapp/
 systemctl daemon-reload
 sudo systemctl enable webapp
 sudo systemctl start webapp
