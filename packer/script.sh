ee2bf94eb18b90dd2106131ef374951425b4dfa8
keerthana <lnu.ke@northeastern.edu>
2023-10-26 20:52:57 -0400

    checks

--- packer/script.sh
+++ packer/script.sh
@@ -24,11 +24,11 @@ EOF
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
  
-sudo apt-get install -y nodejs npm unzip
-sudo npm install -g nodemon
  
 sudo mkdir /opt/csye6225/webapp
-sudo mv /home/admin/webapp1.zip /opt/csye6225/webapp/
+sudo mv ~/webapp1.zip /opt/csye6225/webapp/
+
+
 cd /opt/csye6225/webapp/ || exit
 sudo unzip webapp1.zip
 sudo npm audit fix
