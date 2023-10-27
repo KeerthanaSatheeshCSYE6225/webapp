d83690e9338e3ee53995fa38e92e82ab667b7b7a
keerthana <lnu.ke@northeastern.edu>
2023-10-27 18:53:13 -0400

    commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,8 +2,8 @@ require("dotenv").config(); // Load the dotenv library
 
 module.exports = {
   HOST: process.env.HOST,
-  USER: process.env.USER,
+  USER: process.env.USERNAME,
   PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
+  DB: process.env.DATABASE,
   dialect: "mysql",
 };
