7708b05e847e0163c4bece413067d97cc7510197
Keerthana Satheesh <113073502+Keerthana734@users.noreply.github.com>
2023-11-06 21:57:21 -0500

    ami cloud watch (#57)
    * checks
    
    * Resolved merge conflict by incorporating both suggestions.
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit for cloud watch

--- packer/script.sh
+++ packer/script.sh
@@ -3,7 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get upgrade -y
 
-
 # Install MySQL
 sudo apt-get install -y nodejs npm unzip
 sudo npm install -g nodemon
@@ -35,4 +34,13 @@ sudo chmod -R 750 /opt/csye6225/webapp/
 
 sudo systemctl daemon-reload
 sudo systemctl enable webapp
+
 sudo systemctl start webapp
+
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb -P /tmp/
+sudo dpkg -i /tmp/amazon-cloudwatch-agent.deb
+sudo cp /opt/csye6225/webapp/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl/
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl/cloudwatch-config.json
+
+sudo systemctl restart amazon-cloudwatch-agent
+
