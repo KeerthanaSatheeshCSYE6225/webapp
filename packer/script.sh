964ee841636862cf4d55518c32e39acf47b87085
keerthana <lnu.ke@northeastern.edu>
2023-11-07 00:25:24 -0500

    commit for cloud watch

--- packer/script.sh
+++ packer/script.sh
@@ -44,7 +44,7 @@ sudo systemctl enable webapp
 
 sudo systemctl start webapp
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl/cloudwatch-config.json
+# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl/cloudwatch-config.json
 
 sudo systemctl restart amazon-cloudwatch-agent
 
