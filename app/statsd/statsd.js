3bf53af71a6bc33c4c8239cbe2560e31b088c310
keerthana <lnu.ke@northeastern.edu>
2023-11-08 15:36:03 -0500

    commit for cloud watch

--- app/statsd/statsd.js
+++ app/statsd/statsd.js
@@ -1,1 +1,24 @@
+const StatsD = require("node-statsd");
 
+const client = new StatsD({ host: "127.0.0.1", port: 8125 });
+
+const getStatsD = () => {
+  return (req, res, next) => {
+    const metricName = `api.${req.method.toLowerCase()}.${req.originalUrl}`;
+    client.increment(metricName);
+    next();
+  };
+};
+
+// Function to end the StatsD client
+const endStatsD = () => {
+  return (req, res, next) => {
+    client.close();
+    next();
+  };
+};
+
+module.exports = {
+  getStatsD,
+  endStatsD,
+};
